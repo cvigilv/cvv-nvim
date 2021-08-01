57a6a117410f2c5f0c2c58787833ee8f28a7c430
cvigilv <cvigil2@uc.cl>
2021-08-01 17:39:58 -0400

    🔧 Configurated `QuickScope`
    - Assigned `QuickScope` triggers to 'f', 'F', 't' & 'T'
    - Linked `IncSearch` & `Search` highight groups to `QuickScope`

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -1,4 +1,16 @@
 local execute = vim.cmd
 
+-- Default options
+vim.o.termguicolors = true
+
+-- Overrides autocommand
+execute [[
+	augroup colors_override
+		" QuickScope
+		autocmd ColorScheme * highlight link QuickScopePrimary IncSearch
+		autocmd ColorScheme * highlight link QuickScopeSecondary Search
+    augroup END
+	]]
+
 -- Load colorscheme
 execute "colorscheme tokyonight"
---
+++ lua/plugin/quick-scope/init.lua
@@ -0,0 +1,2 @@
+-- Configuration
+vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
